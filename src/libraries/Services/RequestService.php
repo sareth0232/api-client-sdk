673d9c670f8f8ca3b80ab2cce6abb03605cca2e0
sareth0232 <144675512+sareth0232@users.noreply.github.com>
2023-09-15 16:22:18 +0800

    Update RequestService.php

--- src/libraries/Services/RequestService.php
+++ src/libraries/Services/RequestService.php
@@ -3,69 +3,154 @@
 namespace A8Client\libraries\Services;
 
 use GuzzleHttp\Client;
+use Firebase\JWT\JWT;
 
 class RequestService
 {
 
-	private static $client;
+    const HEADER_AUTHORIZATION = 'Authorization';
+    const HEADER_ACCEPT = 'Accept';
+    const HEADER_CONTENT_TYPE = 'Content-Type';
+    const HEADER_TIMESTAMP = 'Aas-Timestamp';
+    const HEADER_SIGNATURE = 'Aas-Signature';
+    const HEADER_AUTH_TYPE = 'Aas-Auth-Type';
+    const GLUE = '/';
+    const METHOD_GLUE = '_';
+    const BASE_URI = 'https://api7.devcebu.com'; // need to place in config
+    const AUTH_TYPE = 3;
+    const CLIENT_DOMAIN = 'client7.devcebu.com';
+    const API_KEY = 'a8591d1354e958cc842d210a932be3fb';
+    const HTTP_ERRORS = FALSE;
+    const JWT_KEY = 'enMB8ms2Nh8RNz7nRKrGTiMRG9aRPp8G5d';
+    const JWT_ALGORITHM = 'HS256';
+    const USERNAME = 'a8591d1354e958cc842d210a932be3fb';
+    const PASSWORD = '{"domain":"client7.devcebu.com","jwt":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTQ3NzM0NDAsImlkIjo1NiwidXNlcm5hbWUiOiJVU0VSMSJ9.9Jxy3L173Igib6FOyvCXBNs2MzedxFQuVHPj9o_yCx4"}';
+    
+
+	private static $_client;
+    private static $_header;
+    private static $_current_timestamp;
+    private static $_method;
+    private static $_args;
+    private static $_auth_data;
 
 	private static function clientInstance()
-    {
-        static::$client = new Client([
-            'client_domain' => $conf['option']['base_path'],
-            'headers'       => [
-                'API_KEY' => $conf['api_key'],
-                'SECRET_CODE' => $conf['secret_code'],
-                'CLIENT_DOMAIN' => $conf['client_domain'],
-                'AUTH_TYPE' => $conf['option']['auth_type'] ?? $conf['default_auth_type']
-            ],
-            'http_errors'  => $conf['http_errors']
+    {        
+        static::$_current_timestamp = gmdate('Y-m-d\TH:i:s\Z');
+
+        static::$_client = new Client([
+            'base_uri' => self::BASE_URI,
+            'headers' => static::prep_headers(),
+            self::API_KEY => static::auth(),
+            'http_errors'  => self::HTTP_ERRORS
         ]);
     }
     
     public static function __callStatic($method, $args) 
     {
-        static::clientInstance();
+        static::$_method = $method;
+        static::$_args = $args;
 
-        $method == 'put' && $method = 'patch';
+        $method = self::METHOD_GLUE.$method;
 
-        switch ($method) {
-            case 'get':
-                    $param_index = 'query';
-                break;
+        return static::$method();
 
-            case 'post':
-                    $param_index = 'form_params';
-                break;
+    }
 
-            case 'patch':
-                    $param_index = 'form_params';
-                break;
+    private static function send( $method, $path, $body = [] )
+    {
+        try 
+        {
+            static::clientInstance();
 
-            case 'delete':
-                    $param_index = 'query';
-                break;
-            
-            default:
-                    return false;
-                break;
+            $response = static::$_client->request($method, self::GLUE.$path );
+
         }
-
-        $path = isset($args[0]) ? $args[0] : '';
-        $params = isset($args[1])? $args[1] : [];
-
-        $request = static::$client->request($method, $path, [$param_index => $params]);
-        $contents = json_decode($request->getBody()->getContents());
+        catch (\GuzzleHttp\Exception\RequestException $e)
+        {            
+            return $response = $e->getMessage();
+        }
+        
+        $contents = json_decode($response->getBody()->getContents());
 
         if (is_null($contents)) {
-
-            return (object) ['status' => $request->getStatusCode()];
+        
+            return (object) [
+                'code' => $response->getStatusCode(),
+                'message' => $response->getReasonPhrase()
+            ];
         
         } else {
             
             return $contents;
         
         }
+    }
+
+    public static function _get()
+    {
+        static::clientInstance();
+
+        $path = implode("/", static::$_args);
+        
+        return static::send( static::$_method, $path );
+    
+    }
+
+    private static function prep_headers()
+    {
+        static::auth_data();
+        // print_r(base64_encode(self::API_KEY.':'.static::$_auth_data));exit;
+        return [
+            self::HEADER_AUTHORIZATION => 'Basic YTg1OTFkMTM1NGU5NThjYzg0MmQyMTBhOTMyYmUzZmI6eyJkb21haW4iOiJjbGllbnQ3LmRldmNlYnUuY29tIiwiand0IjoiZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SmxlSEFpT2pFMk9UUTNOek0wTkRBc0ltbGtJam8xTml3aWRYTmxjbTVoYldVaU9pSlZVMFZTTVNKOS45Snh5M0wxNzNJZ2liNkZPeXZDWEJOczJNemVkeEZRdVZIUGo5b195Q3g0In0=',
+            self::HEADER_ACCEPT => 'application/json',
+            self::HEADER_CONTENT_TYPE => 'application/x-www-form-urlencoded',
+            self::HEADER_TIMESTAMP => self::$_current_timestamp,
+            self::HEADER_SIGNATURE => '95d068137575e16392c0b42153c7451fd10ad126904a84581dcc88191ed5fac4',
+            self::HEADER_AUTH_TYPE => self::AUTH_TYPE
+        ];
+    }
+
+    private static function auth()
+    {
+        $token = static::generate_token();
+
+        return json_encode((object)[
+            "domain" => self::CLIENT_DOMAIN,
+            "jwt" => $token
+        ]);
+    }
+
+    private static function generate_token( $data = null )
+    {
+        $data['API_TIME'] = time();
+
+        if ( $data && is_array( $data ) ) 
+        {
+            try
+            {
+                return JWT::encode( $data, self::JWT_KEY, self::JWT_ALGORITHM );
+            }
+            catch ( Exception $e ) 
+            {
+                return "Message: ".$e->getMessage();
+            }
+
+        }
+        else
+        {
+            return "Token data is required";
+        }
+
+    }
+
+    private static function auth_data()
+    {
+        static::$_auth_data = json_encode([
+            'domain'=> self::CLIENT_DOMAIN,
+            'username'=> self::USERNAME,
+            'password'=> self::PASSWORD
+        ]);
     }
 	
 }
