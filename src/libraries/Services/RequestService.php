8c97e267b9cbf0768343415a7286e9b9031725d2
sareth0232 <144675512+sareth0232@users.noreply.github.com>
2023-09-15 18:08:02 +0800

    Update RequestService.php

--- src/libraries/Services/RequestService.php
+++ src/libraries/Services/RequestService.php
@@ -8,6 +8,7 @@ use Firebase\JWT\JWT;
 class RequestService
 {
 
+    const CONFIG_PATH = __DIR__.'/../../api_client_sdk_config.ini';
     const HEADER_AUTHORIZATION = 'Authorization';
     const HEADER_ACCEPT = 'Accept';
     const HEADER_CONTENT_TYPE = 'Content-Type';
@@ -16,15 +17,6 @@ class RequestService
     const HEADER_AUTH_TYPE = 'Aas-Auth-Type';
     const GLUE = '/';
     const METHOD_GLUE = '_';
-    const BASE_URI = 'https://api7.devcebu.com'; // need to place in config
-    const AUTH_TYPE = 3;
-    const CLIENT_DOMAIN = 'client7.devcebu.com';
-    const API_KEY = 'a8591d1354e958cc842d210a932be3fb';
-    const API_SECRET_AUTH_3 = '895a744cb823686f485ab1d9ff1b228d9823bef8214b0439954dbebe4c0c5f92';
-    const HTTP_ERRORS = FALSE;
-    const JWT_KEY = 'enMB8ms2Nh8RNz7nRKrGTiMRG9aRPp8G5d';
-    const JWT_ALGORITHM = 'HS256';
-    const TOKEN_TTL = 3600 * 4; 
 
 	private static $_client;
     private static $_header;
@@ -33,19 +25,22 @@ class RequestService
     private static $_args;
     private static $_auth_data;
     private static $_authorization;
+    private static $_config;
 
-    private static function clientInstance()
-    {        
+	private static function clientInstance()
+    {   
+        
+        static::initialize_config();
         static::auth_data();
         static::get_authorization();
 
         static::$_current_timestamp = gmdate('Y-m-d\TH:i:s\Z');
 
         static::$_client = new Client([
-            'base_uri' => self::BASE_URI,
+            'base_uri' => static::$_config['BASE_URI'],
             'headers' => static::prep_headers(),
-            self::API_KEY => static::auth(),
-            'http_errors'  => self::HTTP_ERRORS
+            static::$_config['API_KEY'] => static::auth(),
+            'http_errors'  => static::$_config['HTTP_ERRORS']
         ]);
     }
     
@@ -108,7 +103,7 @@ class RequestService
             self::HEADER_CONTENT_TYPE => 'application/x-www-form-urlencoded',
             self::HEADER_TIMESTAMP => self::$_current_timestamp,
             self::HEADER_SIGNATURE => '95d068137575e16392c0b42153c7451fd10ad126904a84581dcc88191ed5fac4',
-            self::HEADER_AUTH_TYPE => self::AUTH_TYPE
+            self::HEADER_AUTH_TYPE => static::$_config['AUTH_TYPE']
         ];
     }
 
@@ -117,7 +112,7 @@ class RequestService
         $token = static::generate_token( static::$_args );
 
         return json_encode((object)[
-            "domain" => self::CLIENT_DOMAIN,
+            "domain" => static::$_config['CLIENT_DOMAIN'],
             "jwt" => $token
         ]);
     }
@@ -130,7 +125,7 @@ class RequestService
             {
                 $data['API_TIME'] = time();
 
-                return JWT::encode( $data, self::JWT_KEY, self::JWT_ALGORITHM );
+                return JWT::encode( $data, static::$_config['JWT_KEY'], static::$_config['JWT_ALGORITHM'] );
             }
             catch ( Exception $e ) 
             {
@@ -156,19 +151,31 @@ class RequestService
     private static function get_access( $person_id )
     {
         $payload = [
-            'exp' => time() + (int) self::TOKEN_TTL,
+            'exp' => time() + (int) static::$_config['TOKEN_TTL'],
             'id' => $person_id,
         ];
                 
         return JWT::encode($payload, 
-            self::API_SECRET_AUTH_3,
-            self::JWT_ALGORITHM,
+            static::$_config['API_SECRET_AUTH_3'],
+            static::$_config['JWT_ALGORITHM'],
         );
     }
 
     private static function get_authorization()
     {
-        static::$_authorization = base64_encode( self::API_KEY . ':' . static::$_auth_data );
+        static::$_authorization = base64_encode( static::$_config['API_KEY'] . ':' . static::$_auth_data );
+    }
+
+    private static function initialize_config()
+    {
+        if ( file_exists( self::CONFIG_PATH ) ) 
+        {
+            $config = parse_ini_file(self::CONFIG_PATH);
+
+            if ( $config ) {
+                static::$_config = $config;
+            }
+        } 
     }
 	
 }
