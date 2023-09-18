658359e81cccc1aa59e0d02ec40a896547cf0fdd
sareth0232 <144675512+sareth0232@users.noreply.github.com>
2023-09-18 14:32:07 +0800

    Update SecurityService.php

--- src/libraries/Services/SecurityService.php
+++ src/libraries/Services/SecurityService.php
@@ -1,44 +1,8 @@
 <?php
 
-namespace A8Client\libraries\Service;
-
-include_once ( '../../Config.php' );
+namespace A8Client\libraries\Services;
 
 class SecurityService
 {
 	
-	public function __construct()
-	{
-
-
-
-	}
-
-	public function _generate_signature ( array $params ) 
-	{
-
-		try {
-
-			$timestamp = gmdate('Y-m-d\TH:i:s\Z');
-
-			$payload = [
-				$params['method'],
-				$params['url'],
-				$params['query'],
-				$params['form_body'],
-				$timestamp
-			];
-
-			$payload_to_sign = implode("\n", $tmp);
-			
-			return hash_hmac('sha256', $payload_to_sign, $conf['secret_code']);
-
-		} catch ( \Exception $e ) {
-
-			return $e->getMessage();
-
-		}
-
-	}
-
 }
