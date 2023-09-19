94e672c82f3b5f4cdac2c6b32703b359e574bc25
sareth0232 <144675512+sareth0232@users.noreply.github.com>
2023-09-19 19:01:36 +0800

    Create HeadersService.php

---
+++ src/libraries/Services/HeadersService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace A8Client\libraries\Services;
+
+use A8Client\libraries\Services\SecurityService;
+
+class HeadersService
+{
+
+	const HEADER_AUTHORIZATION = 'Authorization';
+    const HEADER_ACCEPT = 'Accept';
+    const HEADER_CONTENT_TYPE = 'Content-Type';
+    const HEADER_TIMESTAMP = 'Aas-Timestamp';
+    const HEADER_SIGNATURE = 'Aas-Signature';
+    const HEADER_AUTH_TYPE = 'Aas-Auth-Type';
+
+    protected $cred;
+    protected $current_timestamp;
+    public $base_path;
+    protected $args;
+    
+    public function __construct ( $cred, $args, $method ) 
+    {
+
+        $this->security_service = new SecurityService($cred, $args, $method);
+        $this->base_path = $this->security_service->base_path;
+        $this->current_timestamp = gmdate('Y-m-d\TH:i:s\Z');
+        $this->cred = $cred;
+        $this->args = $args;
+        
+
+    }
+
+    public function _prep_headers() 
+    {
+        return [
+            $this::HEADER_AUTHORIZATION => 'Basic '.$this->security_service->_get_authorization(),
+            $this::HEADER_ACCEPT => 'application/json',
+            $this::HEADER_CONTENT_TYPE => 'application/x-www-form-urlencoded',
+            $this::HEADER_TIMESTAMP => $this->current_timestamp,
+            $this::HEADER_SIGNATURE => $this->security_service->_sign(),
+            $this::HEADER_AUTH_TYPE => $this->security_service->_get_auth_type()
+        ];
+    }
+
+    public function _auth()
+    {
+        $token = $this->security_service->_generate_token( $this->args );
+
+        return json_encode((object)[
+            "domain" => $this->cred['client_domain'],
+            "jwt" => $token
+        ]);
+    }
+
+}
